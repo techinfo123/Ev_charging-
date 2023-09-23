6759d208fdd31dd153773f7ac0ba32680e4cf211
mehul bansal <87509823+mehul1409@users.noreply.github.com>
2023-09-23 19:39:21 +0530

    Adding maps integration part for registration of private charging stations

---
+++ maps-demo-clustering/pages/index.js
@@ -0,0 +1,55 @@
+import { useState, useCallback } from "react";
+import { GoogleMapProvider } from "@ubilabs/google-maps-react-hooks";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import SuperClusterAlgorithm from "../utils/superClusterAlgorithm";
+import trees from "../data/trees";
+
+const mapOptions = {
+  zoom: 12,
+  center: {
+    lat: 28.535517,
+    lng: 77.391029,
+  },
+};
+
+export default function Index() {
+  const [mapContainer, setMapContainer] = useState(null);
+  const onLoad = useCallback((map) => addMarkers(map), []);
+
+  return (
+    <GoogleMapProvider
+      googleMapsAPIKey={"AIzaSyA5dnKJSLBSEZgmFY5AMPeodUoShhBOQ3A"}
+      options={mapOptions}
+      mapContainer={mapContainer}
+      onLoad={onLoad}
+    >
+      <div ref={(node) => setMapContainer(node)} style={{ height: "100vh" }} />
+    </GoogleMapProvider>
+  );
+}
+
+function addMarkers(map) {
+  const infoWindow = new google.maps.InfoWindow();
+
+  const markers = trees.map(([name, lat, lng]) => {
+    const marker = new google.maps.Marker({ position: { lat, lng } });
+
+    marker.addListener("click", () => {
+      infoWindow.setPosition({ lat, lng });
+      infoWindow.setContent(`
+        <div class="info-window">
+          <h2>${name}</h2>
+        </div>
+      `);
+      infoWindow.open({ map });
+    });
+
+    return marker;
+  });
+
+  new MarkerClusterer({
+    markers,
+    map,
+    algorithm: new SuperClusterAlgorithm({ radius: 200 }),
+  });
+}
