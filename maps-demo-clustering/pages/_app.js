5da4a42467f92dbc0e3134f6852a664ee1233d21
kharedevansh11 <devanshkhare11@gmail.com>
2023-09-25 02:08:43 +0530

    users location, showing nearby charging stations

--- maps-demo-clustering/pages/_app.js
+++ maps-demo-clustering/pages/_app.js
@@ -1,7 +1,13 @@
+import React from 'react'
 import '../styles/globals.css'
+import '../styles/index.css'
 
+import { useEffect } from 'react'
 function MyApp({ Component, pageProps }) {
+   
   return <Component {...pageProps} />
+ 
 }
+
 
 export default MyApp
